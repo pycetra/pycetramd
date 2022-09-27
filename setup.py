49235de19efba545fc6b2f787cb86fc06cbf6f84
pycetra <pycetra@pycetra.com>
2022-09-27 14:11:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="pycetramd",
+    version="0.0.1",
+    author="pycetra",
+    author_email="pycetra@pycetra.com",
+    description="markdown2graph and graph2markdown",
+    packages=setuptools.find_packages(),
+)
