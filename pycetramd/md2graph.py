d9ad321a2523673c0aa61f6290ff9758b3012f85
pycetra <pycetra@pycetra.com>
2023-08-03 13:48:42 +0900

    modify position record

--- pycetramd/md2graph.py
+++ pycetramd/md2graph.py
@@ -1,14 +1,29 @@
 import numpy as np
 import pandas as pd
 
+from pycetramd import datatype as datatype_new
 
-def main(text, config={"sep": "\n", "tab": "  "}):
+
+def main(text, config):
+    dataset = {
+        "node_feature": np.zeros(0, dtype=datatype_new.getDtype("node_feature_np")),
+        "link_feature": np.zeros(0, dtype=datatype_new.getDtype("link_feature_np")),
+        "layer_feature": np.zeros(0, dtype=datatype_new.getDtype("layer_feature_np")),
+        "layer_node_feature": np.zeros(
+            0, dtype=datatype_new.getDtype("layer_node_feature_np")
+        ),
+        "layer_link_feature": np.zeros(
+            0, dtype=datatype_new.getDtype("layer_link_feature_np")
+        ),
+    }
     if "".join(text.split()) == "":
-        return gd.GraphData()
+        return dataset
 
     df = text2dataframe(text, config)
     df = df[df["md"] != ""].reset_index(drop=True)
+
     df["link_flag"] = df["md"].apply(lambda x: 1 if x.count("@") == 2 else 0)
+
     df["level"] = df["md"].apply(
         lambda x: len(x.split("-")[0].split(config["tab"])) - 1
     )
@@ -22,21 +37,8 @@ def main(text, config={"sep": "\n", "tab": "  "}):
     node_f_md = df
 
     size = len(node_f_md)
-    node_f = np.zeros(
-        size,
-        [
-            ("id", np.int64),
-            ("text", "<U200"),
-            ("level", np.int64),
-            ("sortkey", np.int64),
-            ("x", np.float64),
-            ("y", np.float64),
-            ("selecter", np.int64),
-            ("region", "<U200"),
-            ("stylecls", "O"),
-            ("position", "O"),
-        ],
-    )
+    node_f = np.zeros(size, datatype_new.getDtype("node_feature_np"))
+
     node_f["stylecls"] = [[""] for i in range(size)]
     node_f["position"] = [[0] for i in range(size)]
     node_f["id"] = node_f_md["id"]
@@ -47,30 +49,69 @@ def main(text, config={"sep": "\n", "tab": "  "}):
     stdsymbol = node_f_md["text"].apply(
         lambda x: x.split("@")[-1] if x.count("@") == 1 else ""
     )
+    node_f["symbol"] = stdsymbol
+    node_f["stylecls"] = appendStyleCls(
+        node_f["stylecls"], getStyleClsPositionparam(df)
+    )
     node_f["stylecls"] = appendStyleCls(node_f["stylecls"], getStyleClsMarkdownpath(df))
     symbol2id_dict = getIdDict(stdsymbol, node_f["id"])
 
+    index1 = node_f["region"] == "position"
+    index2 = node_f["text"] != "#position"
+    target_index = index1 & index2
+
+    position_array = getPositionParam(node_f, target_index)
+    position_dict = {}
+    for i in position_array:
+        if symbol2id_dict.get(i["symbol"]):
+            position_dict[symbol2id_dict[i["symbol"]]] = [i["x"], i["y"]]
+
+    node_f["symbol"] = [
+        position_array["symbol"][n] if i["symbol"] == "" else i["symbol"]
+        for n, i in enumerate(node_f)
+    ]
+    node_f["position"] = [
+        [i["x"], i["y"]] if i["symbol"] != "" else [0] for i in position_array
+    ]
+    node_f["position"] = [
+        position_dict[i["id"]] if position_dict.get(i["id"]) else i["position"]
+        for i in node_f
+    ]
+    stylecls_position = [
+        "position"
+        if len(i["position"]) == 2 and "position_param" not in i["stylecls"]
+        else ""
+        for i in node_f
+    ]
+    node_f["stylecls"] = appendStyleCls(node_f["stylecls"], stylecls_position)
+    node_f["stylecls"] = [[j for j in i if j != ""] for i in node_f["stylecls"]]
+
+    # markdownをgraphへ変換する
     fromto_list = getFromTo(df, symbol2id_dict)
     size = len(fromto_list)
-    link_f = np.zeros(
-        size,
-        [
-            ("id", np.int64),
-            ("from", np.int64),
-            ("to", np.int64),
-            ("x_from", np.float64),
-            ("y_from", np.float64),
-            ("x_to", np.float64),
-            ("y_to", np.float64),
-            ("selecter", np.int64),
-            ("region", "<U200"),
-            ("stylecls", "O"),
-        ],
-    )
+    link_f = np.zeros(size, datatype_new.getDtype("link_feature_np"))
+    link_f["stylecls"] = [[""] for i in range(size)]
     link_f[["from", "to", "stylecls"]] = fromto_list
     link_f["id"] = [i + 2000 for i in range(size)]
 
-    return (link_f, node_f)
+    dataset["node_feature"] = node_f
+    dataset["link_feature"] = link_f
+
+    return dataset
+
+
+def getPositionParam(node_f, target_index):
+    result = np.zeros(
+        len(node_f), dtype=[("symbol", "U100"), ("x", np.int64), ("y", np.int64)]
+    )
+    result[["x", "y"]] = -1
+    for n, i in enumerate(node_f):
+        if target_index[n]:
+            csv_data = i["text"].split(",")
+            result[n]["symbol"] = csv_data[0]
+            result[n]["x"] = csv_data[1]
+            result[n]["y"] = csv_data[2]
+    return result
 
 
 def appendStyleCls(stylecls, newcls):
@@ -79,6 +120,16 @@ def appendStyleCls(stylecls, newcls):
         tmp = i
         tmp.append(newcls[n])
         result.append(tmp)
+    return result
+
+
+def getStyleClsPositionparam(df):
+    result = []
+    for i in df.to_dict("records"):
+        rec = ""
+        if i["region"].startswith("position"):
+            rec = "position_param"
+        result.append(rec)
     return result
 
 
